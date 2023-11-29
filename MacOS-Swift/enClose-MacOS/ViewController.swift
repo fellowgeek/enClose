7a70611f150ac0df4be9e35e8293e0caac594485
Erfan Reed <erfan.reed@gmail.com>
2023-11-29 10:45:39 -0700

    Enabled web inspector for iOS & allow file:// URLs

--- MacOS-Swift/enClose-MacOS/ViewController.swift
+++ MacOS-Swift/enClose-MacOS/ViewController.swift
@@ -23,23 +23,24 @@ class ViewController: NSViewController, WKUIDelegate, WKScriptMessageHandler, WK
 
     // Override the loadView() function to create and configure the WKWebView
 	override func loadView() {
-		
+
 		let contentController = WKUserContentController()
 		contentController.add(self, name: "enClose")
-		
+
 		let webConfiguration = WKWebViewConfiguration()
 		webConfiguration.userContentController = contentController
-		
+
 		webView = WKWebView(frame: .zero, configuration: webConfiguration)
 		webView.uiDelegate = self
 		webView.navigationDelegate = self
+        webView.configuration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
         if #available(macOS 13.3, iOS 16.4, tvOS 16.4, *) {
             webView.isInspectable = true
         }
 		view = webView
-        
+
 	}
-	
+
     // Override the viewDidLoad() function to load the initial web page
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -49,24 +50,24 @@ class ViewController: NSViewController, WKUIDelegate, WKScriptMessageHandler, WK
 			let request = URLRequest(url: url)
 			webView.load(request)
 		}
-		
+
 	}
-	
+
     // Function to disable user text selection via JavaScript
 	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
 		let javascriptStyle = "var css = '*{-webkit-touch-callout:none;-webkit-user-select:none}'; var head = document.head || document.getElementsByTagName('head')[0]; var style = document.createElement('style'); style.type = 'text/css'; style.appendChild(document.createTextNode(css)); head.appendChild(style);"
 		webView.evaluateJavaScript(javascriptStyle, completionHandler: nil)
 	}
-	
+
     // Function to handle messages received from JavaScript
 	func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-		
+
         // Check if the message body is a string
 		guard let request = message.body as? String else { return }
 
         // Check if the request starts with "ios:"
 		if(request.hasPrefix("ios:") == true) {
-			
+
             // Extract and process the method name and parameters from the request
 			var range: NSRange
             // Find the range from the start of the string to the first "?" character
@@ -77,10 +78,10 @@ class ViewController: NSViewController, WKUIDelegate, WKScriptMessageHandler, WK
             // Find the range from the character after the first "?" to the end of the string
 			range = NSRange(location: Int((request as NSString).range(of: "?").location) + 1, length: (request.count - Int((request as NSString).range(of: "?").location)) - 1)
 			iosParameters = (request as NSString).substring(with: range)
-			
+
             // Initialize an empty dictionary to store key-value pairs
 			queryStringDictionary.removeAll()
-            
+
             // Split the parameters into key-value pairs
 			let urlComponents = iosParameters.components(separatedBy: "&")
 			for keyValuePair: String in urlComponents {
@@ -90,7 +91,7 @@ class ViewController: NSViewController, WKUIDelegate, WKScriptMessageHandler, WK
                 // Add key-value pairs to the dictionary
 				queryStringDictionary[key ?? ""] = value
 			}
-			
+
             // Convert the method name into a selector
 			let selector = NSSelectorFromString(iosMethod)
             // Check if the current object responds to the selector
@@ -98,40 +99,40 @@ class ViewController: NSViewController, WKUIDelegate, WKScriptMessageHandler, WK
                 // If it does, perform the method
 				perform(selector)
 			}
-			
+
             // Check if debug mode is enabled
 			if(debugMode == true) {
 				print("request: \(request)")
                 // Print the original request and the queryStringDictionary
 				print("queryStringDictionary: \(queryStringDictionary)")
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	override var representedObject: Any? {
 		didSet {
 		// Update the view, if already loaded.
 		}
 	}
-	
+
     // A custom "Hello, World!" function which is called from Javascript, it speaks a message
 	let helloWorldSelector = #selector(helloWorld)
 	@objc func helloWorld() {
-		
+
 		let synthesizer = NSSpeechSynthesizer()
 		synthesizer.startSpeaking(queryStringDictionary["message"] ?? "")
-		
+
 		// process nativeCall successCallback function and send data to web UI
 		let successResponse = "You have successfully called a native function from javascript, and you got schwifty!"
-		
+
 		var javaScript: String = ""
 		if let successCallback = queryStringDictionary["successCallback"] {
 			javaScript = "\(successCallback)('\(successResponse)');"
 		}
 		webView.evaluateJavaScript(javaScript, completionHandler: nil)
-		
+
 	}
-	
+
 }
--- iOS-Swift/enClose/ViewController.swift
+++ iOS-Swift/enClose/ViewController.swift
@@ -24,24 +24,34 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler, WK
 
     // Override the loadView() function to create and configure the WKWebView
 	override func loadView() {
-		
+
 		let contentController = WKUserContentController()
 		contentController.add(self, name: "enClose")
-		
+
 		let webConfiguration = WKWebViewConfiguration()
 		webConfiguration.userContentController = contentController
-		
+
 		webView = WKWebView(frame: .zero, configuration: webConfiguration)
 		webView.uiDelegate = self
 		webView.navigationDelegate = self
 		webView.scrollView.bounces = false
+        webView.configuration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
+		// Enable the web inspector for Safari when in debug mode iOS 16.4+
+        if #available(iOS 16.4, *) {
+            if (debugMode == true) {
+                webView.isInspectable = true
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+
 		view = webView
 	}
 
     // Override the viewDidLoad() function to load the initial web page
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+
 		if let url = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "www") {
 			webView.loadFileURL(url, allowingReadAccessTo: url)
 			let request = URLRequest(url: url)
@@ -50,13 +60,13 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler, WK
 
 	}
 
-	
+
     // Function to disable user text selection via JavaScript
 	func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
 		let javascriptStyle = "var css = '*{-webkit-touch-callout:none;-webkit-user-select:none}'; var head = document.head || document.getElementsByTagName('head')[0]; var style = document.createElement('style'); style.type = 'text/css'; style.appendChild(document.createTextNode(css)); head.appendChild(style);"
 		webView.evaluateJavaScript(javascriptStyle, completionHandler: nil)
 	}
-	
+
     // Function to handle messages received from JavaScript
 	func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
 
@@ -65,7 +75,7 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler, WK
 
         // Check if the request starts with "ios:"
 		if(request.hasPrefix("ios:") == true) {
-			
+
             // Extract and process the method name and parameters from the request
 			var range: NSRange
             // Find the range from the start of the string to the first "?" character
@@ -76,10 +86,10 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler, WK
             // Find the range from the character after the first "?" to the end of the string
 			range = NSRange(location: Int((request as NSString).range(of: "?").location) + 1, length: (request.count - Int((request as NSString).range(of: "?").location)) - 1)
 			iosParameters = (request as NSString).substring(with: range)
-			
+
             // Initialize an empty dictionary to store key-value pairs
 			queryStringDictionary.removeAll()
-            
+
             // Split the parameters into key-value pairs
 			let urlComponents = iosParameters.components(separatedBy: "&")
 			for keyValuePair: String in urlComponents {
@@ -104,9 +114,9 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler, WK
 				print("request: \(request)")
 				print("queryStringDictionary: \(queryStringDictionary)")
 			}
-			
+
 		}
-		
+
 	}
 
     // Function to handle memory warnings
@@ -123,20 +133,19 @@ class ViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler, WK
     // A custom "Hello, World!" function which is called from Javascript, it plays a system sound when invoked
 	let helloWorldSelector = #selector(helloWorld)
 	@objc func helloWorld() {
-	
+
 		// play a system sound
 	    AudioServicesPlaySystemSound(1005)
 
-
 		// process nativeCall successCallback function and send data to web UI
 		let successResponse = "You have successfully called a native function from javascript, and you got schwifty!"
-		
+
 		var javaScript: String = ""
 		if let successCallback = queryStringDictionary["successCallback"] {
 			javaScript = "\(successCallback)('\(successResponse)');"
 		}
 		webView.evaluateJavaScript(javaScript, completionHandler: nil)
-		
+
 	}
-	
+
 }
